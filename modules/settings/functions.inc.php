a8a5641c0a9c3e44fbe7a4f8e8f2c13a70ba3a47
Manuel Zavatta <manuel.zavatta@gmail.com>
2017-03-14 18:08:13 +0100

    Functions

---
+++ modules/settings/functions.inc.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Settings - Functions
+ *
+ * @package Coordinator\Modules\Settings
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+?>
