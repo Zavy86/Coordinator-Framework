6614ebbbcc0a559d18cb97c2d1c3716eb4d0c102
Manuel Zavatta <manuel.zavatta@gmail.com>
2017-03-16 14:13:21 +0100

    Setting temporary dashboard

---
+++ modules/settings/dashboard.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Settings - Plugins settings
+ *
+ * @package Rasmotic\Modules\Settings
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.zavynet.org
+ */
+ // include module template
+ require_once(MODULE_PATH."template.inc.php");
+ // check permissions
+ /** @todo check permissions */
+ // set html title
+ $html->setTitle(api_text("settings"));
+
+ /** @todo da rifare bene */
+
+ // make index
+ $index=api_link("?mod=settings&scr=own_profile","<h3>Profilo personale</h3>")."<br>\n";
+ $index.=api_link("?mod=settings&scr=settings_framework","<h3>Impostazioni</h3>")."<br>\n";
+ $index.=api_link("?mod=settings&scr=users_list","<h3>Gestione Utenti</h3>")."<br>\n";
+ $index.=api_link("?mod=settings&scr=groups_list","<h3>Gestione Gruppi</h3>")."<br>\n";
+ $index.=api_link("?mod=settings&scr=sessions_list","<h3>Sessioni</h3>")."<br>\n";
+
+ // build grid object
+ $grid=new Grid();
+ $grid->addRow();
+ $grid->addCol($index,"col-xs-12");
+ // add content to html
+ $html->addContent($grid->render(FALSE));
+ // renderize html page
+ $html->render();
+?>
--- modules/settings/template.inc.php
+++ modules/settings/template.inc.php
@@ -41,6 +41,20 @@
   }
  }
 
+ // groups
+ if(substr(SCRIPT,0,6)=="groups"){
+  // lists
+  $nav->addItem(api_text("groups_list"),"?mod=settings&scr=groups_list");
+  // template operations
+  if(in_array(SCRIPT,array("groups_view","groups_edit")) && $_REQUEST['idGroup']){
+   $nav->addItem(api_text("nav-operations"));
+   $nav->addSubItem(api_text("groups_edit"),"?mod=settings&scr=groups_edit&idGroup=".$_REQUEST['idGroup']);
+  }else{
+   // users add
+   $nav->addItem(api_text("groups_add"),"?mod=settings&scr=groups_edit");
+  }
+ }
+
  // sessions
  if(substr(SCRIPT,0,8)=="sessions"){
   $nav->addItem(api_text("sessions_list"),"?mod=settings&scr=sessions_list");
