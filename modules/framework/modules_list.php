0c306d5f10d90b3ed4173e9903d45da6e23ab15f
Manuel Zavatta <manuel.zavatta@gmail.com>
2017-04-05 16:34:06 +0200

    Module list setup

--- modules/framework/modules_list.php
+++ modules/framework/modules_list.php
@@ -17,6 +17,7 @@
  $table->addHeader(api_text("modules_list-th-name"),"nowrap");
  $table->addHeader(api_text("modules_list-th-installed"),"nowrap text-right");
  $table->addHeader(api_text("modules_list-th-repository"),"nowrap text-right");
+ $table->addHeader("&nbsp;",NULL,16);
  $table->addHeader(api_text("modules_list-th-description"),NULL,"100%");
  $table->addHeader("&nbsp;",NULL,16);
  // get module objects
@@ -30,22 +31,28 @@
   $repository_version=NULL;
   if($module->repository_version_url){$repository_version=file_get_contents($module->repository_version_url."?".rand(1,99999));}
   if(!is_numeric(substr($repository_version,0,1))){$repository_version=NULL;}
+  // check if module is installed
+  if($module->version!="0"){$module_installed=TRUE;}else{$module_installed=FALSE;}
   // check if repository version is updated among source version
   if(api_check_version($module->source_version,$repository_version)>0){$repository_updated=TRUE;}else{$repository_updated=FALSE;}
   // check if source version is updated among installed version
   if(api_check_version($module->version,$module->source_version)>0){$source_updated=TRUE;}else{$source_updated=FALSE;}
-  //
-  if($source_updated){
-   $action_btn=api_link("?mod=framework&scr=submit&act=module_update_database&module=".$module->module,api_text("modules_list-td-update_database",$module->source_version),NULL,"btn btn-warning btn-xs");
-  }elseif($repository_updated){
-   // check for git
-   if(file_exists($module->source_path."/.git/config")){
-    $action_btn=api_link("?mod=framework&scr=submit&act=module_update_source&module=".$module->module,api_text("modules_list-td-update_source"),NULL,"btn btn-info btn-xs",FALSE,api_text("modules_list-td-update_source-confirm"));
+  // check module status
+  if($module_installed){
+   if($source_updated){
+    $action_btn=api_link("?mod=framework&scr=submit&act=module_update_database&module=".$module->module,api_text("modules_list-td-update_database",$module->source_version),NULL,"btn btn-warning btn-xs");
+   }elseif($repository_updated){
+    // check for git
+    if(file_exists($module->source_path."/.git/config")){
+     $action_btn=api_link("?mod=framework&scr=submit&act=module_update_source&module=".$module->module,api_text("modules_list-td-update_source"),NULL,"btn btn-info btn-xs",FALSE,api_text("modules_list-td-update_source-confirm"));
+    }else{
+     $action_btn=api_link("@todo module url".$module->url,api_text("modules_list-td-update_source-manual"),NULL,"btn btn-info btn-xs",FALSE,NULL,NULL,NULL,"_blank");
+    }
    }else{
-    $action_btn=api_link("@todo module url".$module->url,api_text("modules_list-td-update_source-manual"),NULL,"btn btn-info btn-xs",FALSE,NULL,NULL,NULL,"_blank");
+    $action_btn=NULL;
    }
   }else{
-   $action_btn=NULL;
+   $action_btn=api_link("?mod=framework&scr=submit&act=module_setup&module=".$module->module,api_text("modules_list-td-setup"),NULL,"btn btn-success btn-xs");
   }
   //
   $table->addRow();
@@ -53,6 +60,7 @@
   $table->addRowField($module->name,"nowrap");
   $table->addRowField(api_tag("span",$module->version,"label ".($source_updated?"label-warning":"label-success")),"nowrap text-right");
   $table->addRowField(api_tag("span",$repository_version,"label ".($repository_updated?"label-info":"label-success")),"nowrap text-right");
+  $table->addRowField($module->getEnabled(TRUE,FALSE));
   $table->addRowField($module->description,"truncate-ellipsis");
   $table->addRowField($action_btn,"nowrap text-right");
  }
