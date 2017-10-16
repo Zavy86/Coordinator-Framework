503928dd90e70a2c9fb1e6c7797eefc9b4aabe99
Manuel Zavatta <manuel.zavatta@gmail.com>
2017-10-16 16:22:45 +0200

    Cron jobs

--- cron.php
+++ cron.php
@@ -9,19 +9,49 @@
  // include functions
  require_once("functions.inc.php");
  // check token
- //if($_REQUEST['token']!==$GLOBALS['settings']->token_cron){die("TOKEN ERROR");} /** @todo attivare dopo i test */
-
- // delete expired sessions
- $GLOBALS['database']->queryExecute("DELETE FROM `framework_sessions` WHERE `startTimestamp`<'".(time()-36000)."'");
- $GLOBALS['database']->queryExecute("DELETE FROM `framework_sessions` WHERE `lastTimestamp`<'".(time()-$GLOBALS['settings']->sessions_idle_timeout)."'");
-
- api_dump("CRON EXECUTED");
-
+ if($_REQUEST['token']!==$GLOBALS['settings']->token_cron){die("TOKEN ERROR");} /** @todo attivare dopo i test */
  // debug
- if($debug){
-  /*api_dump($GLOBALS['settings']->debug(),"settings");
-  api_dump(get_defined_constants(true)["user"],"contants");
-  api_dump($_SESSION["coordinator_logs"],"logs");*/
-  /** @todo attivare dopo i test */
+ api_dump("CRON STARTED");
+ // definitions
+ $cronjobs_path=array();
+ $cronjobs_hourly_path=array();
+ $cronjobs_daily_path=array();
+ $cronjobs_weekly_path=array();
+ // search and include daily crons
+ if($handle_dir=opendir(ROOT."modules/")){
+  while(FALSE!==($module_dir=readdir($handle_dir))){
+   if($module_dir<>"." && $module_dir<>".." && is_dir(ROOT."modules/".$module_dir)){
+    if($handle_cron=opendir(ROOT."modules/".$module_dir)){
+     while(FALSE!==($cron_job=readdir($handle_cron))){
+      if($cron_job=="cron.inc.php"){$cronjobs_path[]=ROOT."modules/".$module_dir."/".$cron_job;}
+      if($cron_job=="cron.hourly.inc.php"){$cronjobs_daily_path[]=ROOT."modules/".$module_dir."/".$cron_job;}
+      if($cron_job=="cron.daily.inc.php"){$cronjobs_daily_path[]=ROOT."modules/".$module_dir."/".$cron_job;}
+      if($cron_job=="cron.weekly.inc.php"){$cronjobs_weekly_path[]=ROOT."modules/".$module_dir."/".$cron_job;}
+     }
+     closedir($handle_cron);
+    }
+   }
+  }
+  closedir($handle_dir);
  }
+ // include all-times cron jobs
+ api_dump($cronjobs_path,"cronjobs_path");
+ foreach($cronjobs_path as $job_path){if(file_exists($job_path)){include $job_path;}}
+ // include hourly cron jobs
+ if((int)date("i")<5 || $GLOBALS['debug']){
+  api_dump($cronjobs_hourly_path,"cronjobs_hourly_path");
+  foreach($cronjobs_hourly_path as $job_path){if(file_exists($job_path)){include $job_path;}}
+ }
+ // include daily cron jobs
+ if((date("H")==0 && (int)date("i")<5) || $GLOBALS['debug']){
+  api_dump($cronjobs_daily_path,"cronjobs_daily_path");
+  foreach($cronjobs_daily_path as $job_path){if(file_exists($job_path)){include $job_path;}}
+ }
+ // include weekly cron jobs on sunday
+ if((date("w")==0 && date("H")==0 && (int)date("i")<5) || $GLOBALS['debug']){
+  api_dump($cronjobs_weekly_path,"cronjobs_weekly_path");
+  foreach($cronjobs_weekly_path as $job_path){if(file_exists($job_path)){include $job_path;}}
+ }
+ // debug
+ api_dump("CRON EXECUTED");
 ?>
