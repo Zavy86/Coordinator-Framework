877f6b2fd98b1d849159e66007363056353eeb00
Manuel Zavatta <manuel.zavatta@gmail.com>
2018-09-17 16:10:26 +0200

    Sendmail in cron job

--- modules/framework/cron.inc.php
+++ modules/framework/cron.inc.php
@@ -13,6 +13,10 @@
  $GLOBALS['database']->queryExecute("DELETE FROM `framework_sessions` WHERE `lastTimestamp`<'".(time()-$GLOBALS['settings']->sessions_idle_timeout)."'");
  // log
  $logs[]="Expired sessions deleted"; /** @todo verificare ed eventualmente migliorare */
+ // sendmail
+ api_sendmail_process_all();
+ // log
+ $logs[]="Mails processed"; /** @todo verificare ed eventualmente migliorare */
  // debug
  api_dump($logs,"framework");
 ?>
