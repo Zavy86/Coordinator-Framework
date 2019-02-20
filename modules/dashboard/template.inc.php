98492d459b1d61beeb472925a9fba280dfb7f75d
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-02-20 11:31:11 +0100

    Dashboard fix

--- modules/dashboard/template.inc.php
+++ modules/dashboard/template.inc.php
@@ -10,7 +10,7 @@
  $html=new cHTML($module_name);
  // build nav object
  $nav=new cNav("nav-tabs");
- $nav->addItem(api_text("dashboard_view"),"?mod=dashboard&scr=dashboard_view");
+ $nav->addItem(api_text("dashboard_view"),"?mod=dashboard&scr=dashboard");
  $nav->addItem(api_text("dashboard_customize"),"?mod=dashboard&scr=dashboard_customize");
  if(SCRIPT=="dashboard_customize"){$nav->addSubItem(api_text("nav-dashboard-add"),"?mod=dashboard&scr=dashboard_customize&act=addTile");}
  // add nav to html
