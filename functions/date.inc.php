2e6da4ea410dee8631f4914231ba495cc783a5f9
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-03-04 16:27:23 +0100

    Date difference API

--- functions/date.inc.php
+++ functions/date.inc.php
@@ -45,4 +45,32 @@
   return $dt->format($format);
  }
 
+ /**
+  * Date Difference
+  *
+  * @param string $datetime_a Date Time in format YYYY-MM-DD [HH:II:SS]
+  * @param string $datetime_b Date Time in format YYYY-MM-DD [HH:II:SS]
+  * @param string $format Return format (d days | s seconds)
+  * @return double|false Date difference in selected format or false
+  */
+ function api_date_difference($datetime_a=null,$datetime_b=null,$format="d"){
+  // check parameters
+  if(!$datetime_a || !$datetime_b){return false;}
+  if(!in_array($format,array("d","s"))){return false;}
+  // days format
+  if($format=="d"){
+   $datediff_a=strtotime(substr($datetime_a,0,10));
+   $datediff_b=strtotime(substr($datetime_b,0,10));
+   $difference=round(($datediff_b-$datediff_a)/60/60/24);
+  }
+  // seconds format
+  if($format=="s"){
+   $datediff_a=strtotime($datetime_a);
+   $datediff_b=strtotime($datetime_b);
+   $difference=$datediff_b-$datediff_a;
+  }
+  // return
+  return $difference;
+ }
+
 ?>
