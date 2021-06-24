7558275ec39f9418fe477d515f61f8e0bbd51732
Manuel Zavatta <manuel.zavatta@gmail.com>
2021-06-24 12:26:25 +0200

    Groups tree view

--- modules/framework/groups_view.php
+++ modules/framework/groups_view.php
@@ -5,17 +5,69 @@
  * @package Coordinator\Modules\Framework
  * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
  * @link    http://www.coordinator.it
+ *
+ * @var $app strApplication
  */
- // check authorizations
- api_checkAuthorization("framework-groups_manage","dashboard");
- // get object
- $group_obj=new cGroup($_REQUEST['idGroup']);
- if(!$group_obj->id){api_alerts_add(api_text("framework_alert_groupNotFound"),"danger");api_redirect("?mod=".MODULE."&scr=groups_list");}
- // include module template
- require_once(MODULE_PATH."template.inc.php");
- // set application title
- $app->setTitle(api_text("groups_view",$group_obj->name));
- die("@todo fare vista in modalità organigramma");
- // debug
- api_dump($group_obj,"group_obj");
-?>
+// check authorizations
+api_checkAuthorization("framework-groups_manage","dashboard");
+// get objects
+$group_obj=new cGroup($_REQUEST['idGroup']);
+$fathergroup_obj=new cGroup($group_obj->fkGroup);
+// check objects
+if(!$group_obj->id){api_alerts_add(api_text("framework_alert_groupNotFound"),"danger");api_redirect("?mod=".MODULE."&scr=groups_list");}
+// include module template
+require_once(MODULE_PATH."template.inc.php");
+// set application title
+$app->setTitle(api_text("groups_view",$group_obj->name));
+// make content
+function groups_view_makeContent($group_obj){
+	$content=api_link(api_url(["scr"=>"groups_view","idGroup"=>$group_obj->id]),$group_obj->name,$group_obj->description,"hidden-link text-strong",true);
+	$content.="<hr>".api_tag("div",implode("<br>",groups_view_getArrayOfAssignedUsers($group_obj)));
+	return $content;
+}
+// get assigned users
+function groups_view_getArrayOfAssignedUsers($group_obj){
+	$users_array=array();
+	foreach($group_obj->getAssignedUsers() as $assignedUser_f){
+		$user_obj=new cUser($assignedUser_f->id);
+		$users_array[]=api_link(api_url(["scr"=>"users_view","idUser"=>$user_obj->id]),$user_obj->fullname,null,"hidden-link",false,null,null,null,"_blank");
+	}
+	sort($users_array);
+	return $users_array;
+}
+// check for father
+if($fathergroup_obj->id){
+	// build tree starting from father
+	$tree=new strTree(groups_view_makeContent($fathergroup_obj));
+}else{
+	// build tree starting from father
+	$tree=new strTree(api_icon("fa-home",api_text("groups_view-home")));
+}
+// build group tree starting from father
+$group_tree=$tree->addLeaf(groups_view_makeContent($group_obj));
+// cycle all subgroups
+foreach(api_availableGroups($group_obj->id) as $subgroup_fobj){
+	// add content to leaf
+	$sub_tree=$group_tree->addLeaf(groups_view_makeContent($subgroup_fobj));
+	// cycle all subgroups
+	foreach(api_availableGroups($subgroup_fobj->id) as $subsubgroup_fobj){
+		// add content to leaf
+		$sub_sub_tree=$sub_tree->addLeaf(groups_view_makeContent($subsubgroup_fobj));
+		// check for other subgroups
+		if(count(api_availableGroups($subsubgroup_fobj->id))){
+			$sub_sub_tree->addLeaf(api_link(api_url(["scr"=>"groups_view","idGroup"=>$subsubgroup_fobj->id]),api_icon("fa-list",api_text("groups_view-subwalk"),"hidden-link")));
+		}
+	}
+}
+// build grid object
+$grid=new strGrid();
+$grid->addRow();
+$grid->addCol((is_object($tree)?$tree->render():$group_tree->render()),"col-xs-12");
+// add content to application
+$app->addContent($grid->render());
+// renderize application
+$app->render();
+// debug
+api_dump($group_obj,"group object");
+api_dump($fathergroup_obj,"father group object");
+api_dump($tree);
