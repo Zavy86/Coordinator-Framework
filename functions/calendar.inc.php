69a68135b5c89358ffe32671f391ac126405c522
Manuel Zavatta <manuel.zavatta@gmail.com>
2022-02-03 08:52:38 +0100

    Months calendar

--- functions/calendar.inc.php
+++ functions/calendar.inc.php
@@ -13,8 +13,25 @@
  * @return array Weekly Days
  */
 function api_calendar_weekly_days(){
-	// definitions
-	$days_array=array(1=>"monday",2=>"tuesday",3=>"wednesday",4=>"thursday",5=>"friday",6=>"saturday",7=>"sunday");
-	// return
-	return $days_array;
+	return array(1=>"monday",2=>"tuesday",3=>"wednesday",4=>"thursday",5=>"friday",6=>"saturday",7=>"sunday");
+}
+
+/**
+ * Calendar Months
+ *
+ * @return array Months
+ */
+function api_calendar_months(){
+	return array(1=>"january",2=>"february",3=>"march",4=>"april",5=>"may",6=>"june",7=>"july",8=>"august",9=>"september",10=>"october",11=>"november",12=>"december");
+}
+
+/**
+ * Calendar Previous month
+ *
+ * @return int Previous month
+ */
+function api_calendar_previous_month($month){
+	if($month<1||$month>12){return 1;}
+	if(--$month<1){return 12;}
+	return $month;
 }
